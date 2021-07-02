334e3ea3386aa281078d91abec11a62bb6289764
Fluxticks <benjigarment.appdev@gmail.com>
2021-07-03 01:26:04 +0200

    Added vars to constructor

--- src/ReactableMenu.py
+++ src/ReactableMenu.py
@@ -7,7 +7,15 @@ from discord.ext.commands import Context
 class ReactableMenu(Embed):
 
     def __init__(self, **kwargs):
+        self.react_add_func = kwargs.pop("add_func", None)
+        self.react_remove_func = kwargs.pop("remove_func", None)
         super().__init__(**kwargs)
+        self.id = None
+        self.message = None
+        self.options = {}
+
+    def __str__(self) -> str:
+        return super().__str__()
 
     def add_option(self, emoji, descriptor) -> bool:
         pass
@@ -26,4 +34,3 @@ class ReactableMenu(Embed):
 
     def from_dict(self, data: Dict):
         pass
-
